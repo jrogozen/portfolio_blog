e6ef2ccda4d9016599e105fe0606894d90c4c408
Jon Rogozen <jon.rogozen@gmail.com>
2014-10-14 09:09:05 -0700

    add top br

--- app/assets/javascripts/home_transitions.js
+++ app/assets/javascripts/home_transitions.js
@@ -1,5 +1,27 @@
 $(document).ready(function(){
 
-$('.portfolio-slide').slick();
+  function goToByScroll(id){
+      // Remove "link" from the ID
+      // Scroll
+    $('html,body').animate({
+      scrollTop: $("."+id).offset().top}, 'slow');
+  }
+
+
+  $('.portfolio-slide').slick();
+
+  $('.back-to-top').click(function(){
+    $('html,body').animate({ scrollTop: 0 }, 'slow');
+  });
+
+  $('.skills-link').click(function(){
+    goToByScroll("skills");
+  });
+
+  $('.portfolio-link').click(function(){
+    goToByScroll("portfolio");
+  });
+
+
 
 });
